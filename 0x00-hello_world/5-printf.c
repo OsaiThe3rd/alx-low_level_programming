80740dd0f61b59c0c0490938ab6e63fa05bf4dbc
OsaiThe3rd <osaithe3rd@gmail.com>
2023-06-15 19:30:05 -0700

    c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,2 +1,13 @@
-#!/bin/bash
-gcc -E $CFILE -o c
+#include <stdio.h>
+
+/**
+ * main - using print
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+
+	return (0);
+}

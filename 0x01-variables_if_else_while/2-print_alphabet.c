dca90bfe8980b3c3c902f73beeca899ed2bdd473
OsaiThe3rd <osaithe3rd@gmail.com>
2023-06-16 17:46:58 -0700

    if

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - alphabet
+ * Return: 0
+ */
+int main (void)
+{
+	char s;
+
+	for (s = 'a'; s <= 'z'; s++)
+	{
+		putchar(s);
+	}
+	putchar('\n');
+	return (0);
+}

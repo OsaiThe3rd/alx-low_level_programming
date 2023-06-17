f127a8046b940796f8b8c0c5094e087f578529bb
OsaiThe3rd <osaithe3rd@gmail.com>
2023-06-16 18:13:30 -0700

    if

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - alphabet exceptions
+ * Return: 0
+ */
+int main(void)
+{
+	char s;
+
+	for (s = 'a'; s <= 'z'; s++)
+	{
+		if (s != 'q' && s !='e')
+		{
+			putchar(s);
+		}
+	}
+	putchar('\n');
+	return (0);
+}

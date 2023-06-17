bda5f9decf40edba2b90cd329ca064948f9382b3
OsaiThe3rd <osaithe3rd@gmail.com>
2023-06-16 19:15:39 -0700

    if

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - base ten
+ * Return: 0
+ */
+int main(void)
+{
+	int s;
+	
+	for ( s = '0'; s <= '9'; s++)
+	{
+		printf("%d, s");
+	}
+	printf('\n');
+	return (0);
+}

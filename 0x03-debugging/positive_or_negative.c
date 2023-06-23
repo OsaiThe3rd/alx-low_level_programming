57cab399b08aa69f62231e3f21d16286c37d10ad
OsaiThe3rd <osaithe3rd@gmail.com>
2023-06-23 16:07:59 -0700

    alx

--- 0x03-debugging/positive_or_negative.c
+++ 0x03-debugging/positive_or_negative.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * positive_or_negative - is to check if a number is positive or negative 
+ * positive_or_negative - is to check if a number is positive or negative
  * @i : number to be checked
  * Return:0
  */
@@ -15,9 +15,9 @@ else if (i > 0)
 printf("%d is %s\n", i, "positive");
 }
 else
-{		
+{
 printf("%d is %s\n", i, "zero");
-}	
+}
 return;
 
 }

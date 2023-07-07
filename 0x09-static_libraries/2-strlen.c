8b6caeff13ba0fcff7cda07b90680ba7544bfab5
OsaiThe3rd <osaithe3rd@gmail.com>
2023-07-07 16:14:23 -0700

    finish

---
+++ 0x09-static_libraries/2-strlen.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _strlen - A function to return the length of a variabe
+ * @s: A pointer variable
+ * Return: 0 - 1
+ */
+
+int _strlen(char *s)
+{
+	int strlens = 0;
+
+	while (*s != '\0')
+	{
+		strlens++;
+		s++;
+	}
+
+	return (strlens);
+}
